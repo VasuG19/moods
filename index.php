3edb0814fce0aa5d1c213ce76944ada87b4ef3e8
VasuG19 <99899682+VasuG19@users.noreply.github.com>
2023-06-01 09:18:16 +0100

    updates

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ $client->addScope ("https://www.googleapis.com/auth/userinfo.email");
 
 if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
 	$client->setAccessToken($_SESSION['access_token']);
-    $redirect = 'http://' . $_SERVER['HTTP_HOST'].'/index.html';
+    $direct = 'http://' . $_SERVER['HTTP_HOST'].'/index.html';
+	header('Location: ' . filter_var ($direct, FILTER_SANITIZE_URL));
 } else {
 	$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php'; 
 	header('Location: ' . filter_var ($redirect_uri, FILTER_SANITIZE_URL));
