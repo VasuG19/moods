1bc3e3253ecf2826a1aa166946bb76040e3ae9c6
VasuG19 <99899682+VasuG19@users.noreply.github.com>
2023-06-01 09:14:12 +0100

    oAuth

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+// much of this code is taken from the workshops during lectures
+
+require_once __DIR__.'/vendor/autoload.php';
+session_start();
+
+$client = new Google\Client();
+$client->setAuthConfig('client_secret.json');
+$client->addScope ("https://www.googleapis.com/auth/userinfo.email");
+
+if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+	$client->setAccessToken($_SESSION['access_token']);
+    $redirect = 'http://' . $_SERVER['HTTP_HOST'].'/index.html';
+} else {
+	$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php'; 
+	header('Location: ' . filter_var ($redirect_uri, FILTER_SANITIZE_URL));
+}
+
+if ($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['signOut'])){
+	$client->revokeToken($_SESSION['access_token']);
+	session_destroy();
+	$redirect = 'http://' . $_SERVER['HTTP_HOST'].'/index.php';
+	header('Location: ' . filter_var ($redirect, FILTER_SANITIZE_URL));
+}
+?>
